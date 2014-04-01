78700eb8b0eff2d287bba3c9c3650e037290a6c5
Jacob Howard <jacob@havoc.io>
2014-04-01 23:43:01 +0300

    Removed old python version.

--- xeno/__init__.py
+++
@@ -1,7 +0,0 @@
-# Check the python version before proceeding too far
-from xeno.version import check_python_version, XENO_VERSION, STRINGIFY_VERSION
-check_python_version()
-
-
-# Export the xeno version
-__version__ = STRINGIFY_VERSION(XENO_VERSION)
