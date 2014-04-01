54ac03add3af1d00da03fc4a757e0a36d6ad85dc
Jacob Howard <jacob@havoc.io>
2014-04-01 23:17:39 +0300

    Removed some Travis debugging.

--- scripts/ci-setup-ssh.sh
+++ scripts/ci-setup-ssh.sh
@@ -5,6 +5,3 @@ sudo start ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -N "" -q
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 ssh-keyscan -t rsa localhost >> ~/.ssh/known_hosts
-
-cat ~/.bashrc
-cat ~/.profile
