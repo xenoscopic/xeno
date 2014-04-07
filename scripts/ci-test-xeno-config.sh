236dae2808b6c8f8ee824c3ee4aae79b11dfe42d
Jacob Howard <jacob@havoc.io>
2014-04-07 20:40:35 +0300

    Added some initial, non-trivial unit tests.

---
+++ scripts/ci-test-xeno-config.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+
+# Tests the 'xeno config' command
+
+
+oneTimeSetUp()
+{
+  # Set the XENO_CONFIGURATION_PATH environment variable so that we don't
+  # pollute the user's actual configuration file
+  export XENO_CONFIGURATION_FILE="/tmp/xeno.conf"
+}
+
+
+testExitCodes()
+{
+  # Check that running without any help flag results in an error exit code
+  ./xeno config > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno config by itself should exit with code 0" 0 ${result}
+
+
+  # Check that running with a short help flag results in a non-error exit code
+  ./xeno config -h > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno config with -h should exit with code 0" 0 ${result}
+
+  # Check that running with a long help flag results in a non-error exit code
+  ./xeno config --help > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno config with --help should exit with code 0" 0 ${result}
+
+
+  # Check that running with a short clear flag results in a non-error exit code
+  ./xeno -c > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno config with -c should exit with code 1" 1 ${result}
+
+  # Check that running with a long help flag results in a non-error exit code
+  ./xeno --clear > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with --clear should exit with code 1" 1 ${result}
+}
+
+
+testSetRead()
+{
+  # Check that a value can be set and read back
+  ./xeno config test.value "something" > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno config should be able to set and read values" \
+    $(./xeno config test.value) "something"
+}
+
+
+testClear()
+{
+  # Check that a value can be set and cleared with the short clear flag
+  ./xeno config test.value "something" > /dev/null 2>&1
+  ./xeno config -c test.value > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno config with -c should clear values" 0 ${result}
+
+  # Check that a value can be set and cleared with the long clear flag
+  ./xeno config test.value "something" > /dev/null 2>&1
+  ./xeno config --clear test.value > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno config with --clear should clear values" 0 ${result}
+}
--- scripts/ci-test-xeno.sh
+++ scripts/ci-test-xeno.sh
@@ -1,9 +1,71 @@
 #!/bin/sh
 
 
-echo "Path is:"
-echo "$PATH"
-echo "working directory is:"
-pwd
-ls
-echo "All done here"
+# Tests the 'xeno' command
+
+
+testExitCodes()
+{
+  # Check that running without any help flag results in an error exit code
+  ./xeno > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno by itself should exit with code 1" 1 ${result}
+
+
+  # Check that running with an unknown subcommand results in an error exit code
+  ./xeno boguscommand > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with an unknown subcommand should exit with code 1" \
+    1 ${result}
+
+
+  # Check that running with a short version flag results in a non-error exit
+  # code
+  ./xeno -v > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with -v should exit with code 0" 0 ${result}
+
+  # Check that running with a long version flag results in a non-error exit code
+  ./xeno --version > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with --version should exit with code 0" 0 ${result}
+
+  # Check that running with a short version flag with a subcommand results in a
+  # non-error exit code
+  ./xeno -v config > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with -v and a subcommand should exit with code 0" \
+    0 ${result}
+
+  # Check that running with a long version flag with a subcommand results in a
+  # non-error exit code
+  ./xeno --version config > /dev/null 2>&1
+  result=$?
+  assertEquals \
+    "xeno with --version and a subcommand should exit with code 0" 0 ${result}
+
+
+  # Check that running with a short help flag results in a non-error exit code
+  ./xeno -h > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with -h should exit with code 0" 0 ${result}
+
+  # Check that running with a long help flag results in a non-error exit code
+  ./xeno --help > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with --help should exit with code 0" 0 ${result}
+
+  # Check that running with a short help flag with a subcommand results in a
+  # non-error exit code
+  ./xeno -h config > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with -h and a subcommand should exit with code 0" \
+    0 ${result}
+
+  # Check that running with a long help flag with a subcommand results in a
+  # non-error exit code
+  ./xeno --help config > /dev/null 2>&1
+  result=$?
+  assertEquals "xeno with --help and a subcommand should exit with code 0" \
+    0 ${result}
+}
