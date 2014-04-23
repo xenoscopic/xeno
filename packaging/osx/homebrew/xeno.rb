95355bf67ab7636a51ac3605a6ce5c37ec7d0e75
Jacob Howard <jacob@havoc.io>
2014-04-23 17:19:47 +0300

    Updated homebrew formula to version 1.0.3.

--- packaging/osx/homebrew/xeno.rb
+++ packaging/osx/homebrew/xeno.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Xeno < Formula
   homepage 'http://xeno.io'
-  url 'https://github.com/havoc-io/xeno/archive/1.0.2.tar.gz'
-  version '1.0.2'
-  sha1 'f3f7d7f2aa0aa8cb65cc11db05f30fe0680bae3f'
+  url 'https://github.com/havoc-io/xeno/archive/1.0.3.tar.gz'
+  version '1.0.3'
+  sha1 '32fe88399a0acb915881dae000d5904575cd9585'
 
   def install
     bin.install 'xeno'
